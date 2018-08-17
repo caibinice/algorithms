cdfeefd6f8f45627d8bc3267c7d3935972124809
fish <391459189@qq.com>
2018-08-17 18:20:56 +0900

    本周作业

---
+++ src/main/java/algorithms/leetcode/MinimumPathSum.java
@@ -0,0 +1,61 @@
+package algorithms.leetcode;
+
+public class MinimumPathSum {
+
+    /**
+     * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+     * <p>
+     * Note: You can only move either down or right at any point in time.
+     * <p>
+     * Example:
+     * <p>
+     * Input:
+     * [
+     * [1,3,1],
+     * [1,5,1],
+     * [4,2,1]
+     * ]
+     * Output: 7
+     * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+     */
+    //dp
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        if (row == 0) return 0;
+        int col = grid[0].length;
+        if (col == 0) return 0;
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+                }
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    public int minPathSumV2(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (i == 0)
+                    grid[i][j] += grid[i][j - 1];
+                else if (j == 0)
+                    grid[i][j] += grid[i - 1][j];
+                else
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+
+}
